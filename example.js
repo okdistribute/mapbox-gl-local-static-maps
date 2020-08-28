c1b4c75a6ac37c79df8b4f628c5e57d2029d67c8
okdistribute <633012+okdistribute@users.noreply.github.com>
2020-08-28 14:52:10 -0700

    fix: use hidden-mapbox & apply standard

--- example.js
+++ example.js
@@ -1,33 +1,33 @@
 // @ts-ignore
-const html = require("yo-yo");
-const mapboxgl = require("mapbox-gl");
-const startMapService = require("./");
+const html = require('yo-yo')
+const mapboxgl = require('mapbox-gl')
+const startMapService = require('./')
 
-startMapService("./sw.js");
+startMapService('./sw.js')
 
-const root = html`<div />`;
-document.body.appendChild(root);
+const root = html`<div />`
+document.body.appendChild(root)
 
 const values = {
   accessToken:
-    "pk.eyJ1IjoiZGlnaWRlbSIsImEiOiJuM3FabmNFIn0._gF6262MSzePWUChu4S9PA",
+    'pk.eyJ1IjoiZGlnaWRlbSIsImEiOiJuM3FabmNFIn0._gF6262MSzePWUChu4S9PA',
   longitude: 0,
   latitude: 0,
   zoom: 0,
-  style: "mapbox://styles/mapbox/outdoors-v11",
+  style: 'mapbox://styles/mapbox/outdoors-v11',
   width: 300,
   height: 300,
-  pixelRatio: window.devicePixelRatio,
-};
+  pixelRatio: window.devicePixelRatio
+}
 
 /** @param {{id: keyof typeof values, type?: string}} options */
-const input = ({ id, type = "text" }) => {
+const input = ({ id, type = 'text' }) => {
   /** @param {{target: HTMLInputElement}} event */
-  function handleInput({ target }) {
-    if (!target || target.value === "") return;
+  function handleInput ({ target }) {
+    if (!target || target.value === '') return
     // @ts-ignore
-    values[id] = type === "number" ? target.valueAsNumber : target.value;
-    updateMap();
+    values[id] = type === 'number' ? target.valueAsNumber : target.value
+    updateMap()
   }
   return html`<div class="mb-2">
     <label class="block text-gray-700 text-sm mb-1" for="${id}">
@@ -40,18 +40,18 @@ const input = ({ id, type = "text" }) => {
       value="${getValue(id)}"
       oninput=${handleInput}
     />
-  </div>`;
-};
+  </div>`
+}
 
-function updateMap() {
-  console.log("update map", values);
+function updateMap () {
+  console.log('update map', values)
   map.jumpTo({
     center: [values.longitude, values.latitude],
-    zoom: values.zoom,
-  });
+    zoom: values.zoom
+  })
   try {
-    new URL(values.style);
-    map.setStyle(values.style);
+    new URL(values.style)
+    map.setStyle(values.style)
   } catch (e) {}
 }
 
@@ -59,11 +59,11 @@ function updateMap() {
  * @param {keyof typeof values} id
  * @returns {number | string}
  */
-function getValue(id) {
-  return values[id];
+function getValue (id) {
+  return values[id]
 }
 
-function generateImageUrl() {
+function generateImageUrl () {
   const {
     longitude,
     latitude,
@@ -72,34 +72,34 @@ function generateImageUrl() {
     width,
     height,
     style,
-    pixelRatio,
-  } = values;
-  return `/static-maps/${longitude},${latitude},${zoom}/${width}x${height}@${pixelRatio}x?accessToken=${accessToken}&style=${style}`;
+    pixelRatio
+  } = values
+  return `/static-maps/${longitude},${latitude},${zoom}/${width}x${height}@${pixelRatio}x?accessToken=${accessToken}&style=${style}`
 }
 
 const form = () => html`
   <div class="w-full max-w-xs">
     <form class="bg-white shadow-md rounded px-8 pt-6 pb-8">
       <h3 class="block text-gray-700 text-sm font-bold mb-2">Parameters</h3>
-      ${input({ id: "accessToken" })} ${input({ id: "style" })}
+      ${input({ id: 'accessToken' })} ${input({ id: 'style' })}
       <div class="grid grid-cols-2 gap-4">
-        ${input({ id: "longitude", type: "number" })}
-        ${input({ id: "latitude", type: "number" })}
+        ${input({ id: 'longitude', type: 'number' })}
+        ${input({ id: 'latitude', type: 'number' })}
       </div>
-      ${input({ id: "zoom", type: "number" })}
+      ${input({ id: 'zoom', type: 'number' })}
       <div class="grid grid-cols-2 gap-4">
-        ${input({ id: "width", type: "number" })}
-        ${input({ id: "height", type: "number" })}
+        ${input({ id: 'width', type: 'number' })}
+        ${input({ id: 'height', type: 'number' })}
       </div>
-      ${input({ id: "pixelRatio", type: "number" })}
+      ${input({ id: 'pixelRatio', type: 'number' })}
       <div class="flex items-center justify-between mt-6">
         <button
           class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="button"
           onclick=${() => {
-            html.update(imageEl, mapImage(""));
-            html.update(imageEl, mapImage(generateImageUrl()));
-          }}
+    html.update(imageEl, mapImage(''))
+    html.update(imageEl, mapImage(generateImageUrl()))
+  }}
         >
           Generate Image
         </button>
@@ -107,14 +107,14 @@ const form = () => html`
     </form>
     <p class="text-center text-gray-500 text-xs"></p>
   </div>
-`;
+`
 
 /** @param {string} src */
-const mapImage = (src) => html`<img src=${src} />`;
+const mapImage = (src) => html`<img src=${src} />`
 
-const mapEl = html`<div class="absolute w-full h-full" />`;
-const formEl = form();
-const imageEl = mapImage(generateImageUrl());
+const mapEl = html`<div class="absolute w-full h-full" />`
+const formEl = form()
+const imageEl = mapImage(generateImageUrl())
 
 html.update(
   root,
@@ -127,21 +127,21 @@ html.update(
       ${imageEl}
     </div>
   `
-);
+)
 
 const map = new mapboxgl.Map({
   accessToken: values.accessToken,
   center: [values.longitude, values.latitude],
   zoom: values.zoom,
   style: values.style,
-  container: mapEl,
-});
+  container: mapEl
+})
 
-map.on("moveend", (e) => {
-  const center = map.getCenter();
-  const zoom = map.getZoom();
-  values.longitude = Math.round(center.lng * 10000) / 10000;
-  values.latitude = Math.round(center.lat * 10000) / 10000;
-  values.zoom = Math.round(zoom * 100) / 100;
-  html.update(formEl, form());
-});
+map.on('moveend', (e) => {
+  const center = map.getCenter()
+  const zoom = map.getZoom()
+  values.longitude = Math.round(center.lng * 10000) / 10000
+  values.latitude = Math.round(center.lat * 10000) / 10000
+  values.zoom = Math.round(zoom * 100) / 100
+  html.update(formEl, form())
+})
--- index.js
+++ index.js
@@ -1,99 +1,45 @@
-var mapboxgl = require("mapbox-gl");
+var Map = require('hidden-mapbox')
 
 /** @typedef {import('./types').MsgReqMap} MsgReqMap */
 /** @typedef {import('./types').MsgMapReady} MsgMapReady */
 
 // Names must match those in sw.js (we are not packaging sw.js so no requires)
-const MSG_REQ_MAP = "request-map";
-const MSG_MAP_READY = "map-ready";
-const MSG_REGISTER = "register-map-renderer";
-const CACHE_NAME = "map-images";
-const originalPixelRatio = window.devicePixelRatio;
+const MSG_REQ_MAP = 'request-map'
+const MSG_MAP_READY = 'map-ready'
+const MSG_REGISTER = 'register-map-renderer'
+const CACHE_NAME = 'map-images'
 
 /**
  * @param {string} scriptURL
  */
-module.exports = async function startMapService(scriptURL) {
-  const serviceWorker = await registerServiceWorker(scriptURL);
+module.exports = async function startMapService (scriptURL) {
+  const serviceWorker = await registerServiceWorker(scriptURL)
 
   /** @type {HTMLElement} */
-  const mapDiv = createMapDiv();
-  const map = new mapboxgl.Map({
-    container: mapDiv,
-    preserveDrawingBuffer: true,
-  });
-  const cache = await caches.open(CACHE_NAME);
+  const map = new Map()
+  const cache = await caches.open(CACHE_NAME)
 
-  serviceWorker.addEventListener("message", async ({ data: message }) => {
-    if (!message || message.type !== MSG_REQ_MAP) return;
-    console.log("Map req", message.mapOptions);
+  serviceWorker.addEventListener('message', async ({ data: message }) => {
+    if (!message || message.type !== MSG_REQ_MAP) return
+    console.log('Map req', message.mapOptions)
     const {
       requestId,
       cacheKey,
-      mapOptions,
-    } = /** @type {MsgReqMap} */ (message);
-    const image = await getMapImage(mapOptions);
-    console.log("image ready, caching", cacheKey);
-    await cache.put(cacheKey, new Response(image, { status: 200 }));
+      mapOptions
+    } = /** @type {MsgReqMap} */ (message)
+    const image = await map.getMapImage(mapOptions)
+    console.log('image ready, caching', cacheKey)
+    await cache.put(cacheKey, new Response(image, { status: 200 }))
     const msgResponse = /** @type {MsgMapReady} */ ({
       requestId,
-      type: MSG_MAP_READY,
-    });
+      type: MSG_MAP_READY
+    })
     if (serviceWorker.controller) {
       // Inform the service worker the map is ready
-      serviceWorker.controller.postMessage(msgResponse);
+      serviceWorker.controller.postMessage(msgResponse)
     }
-  });
-
-  /**
-   * @param {import('./types').MapOptions} mapOptions
-   * @returns {Promise<Blob?>}
-   */
-  async function getMapImage({
-    accessToken,
-    width,
-    height,
-    style,
-    center,
-    zoom,
-    pixelRatio,
-  }) {
-    return new Promise((resolve) => {
-      if (accessToken) mapboxgl.accessToken = accessToken;
-      mapDiv.style.width = width + "px";
-      mapDiv.style.height = height + "px";
-      // @ts-ignore This is actually writable in Chrome, and can be used to
-      // trick Mapbox into rendering higher-res
-      window.devicePixelRatio = pixelRatio;
-      map.resize();
-      if (style) map.setStyle(style);
-      console.log("set style to", style);
-      map.jumpTo({ center, zoom });
-      map.on("idle", () => {
-        // @ts-ignore
-        window.devicePixelRatio = originalPixelRatio;
-        console.log("map ready");
-        map.getCanvas().toBlob((blob) => {
-          resolve(blob);
-        });
-      });
-    });
-  }
-
-  /**
-   * Returns a hidden div element for the map
-   * @returns {HTMLElement}
-   */
-  function createMapDiv() {
-    const mapDiv = document.createElement("div");
-    document.body.appendChild(mapDiv);
-    mapDiv.style.visibility = "hidden";
-    mapDiv.style.position = "absolute";
-    mapDiv.style.width = "500px";
-    mapDiv.style.height = "500px";
-    return mapDiv;
-  }
-};
+  })
+}
 
 /**
  * Register a service worker and resolve when it is activated.
@@ -102,55 +48,54 @@ module.exports = async function startMapService(scriptURL) {
  * @param {string} url
  * @returns {Promise<ServiceWorkerContainer>}
  */
-async function registerServiceWorker(url) {
-  if (!("serviceWorker" in navigator))
-    throw new Error("ServiceWorkers not supported by this browser");
-  const { serviceWorker: serviceWorkerContainer } = navigator;
-  const registration = await serviceWorkerContainer.register(url);
+async function registerServiceWorker (url) {
+  if (!('serviceWorker' in navigator)) { throw new Error('ServiceWorkers not supported by this browser. Are you sure you are on localhost?') }
+  const { serviceWorker: serviceWorkerContainer } = navigator
+  const registration = await serviceWorkerContainer.register(url)
 
   /** @param {ServiceWorker} sw */
-  function registerClient(sw) {
-    console.log("register client");
-    sw.postMessage({ type: MSG_REGISTER });
+  function registerClient (sw) {
+    console.log('register client')
+    sw.postMessage({ type: MSG_REGISTER })
   }
 
-  serviceWorkerContainer.addEventListener("controllerchange", (e) => {
-    console.log("controllerchange", e.target);
-    if (!serviceWorkerContainer.controller) return;
-    registerClient(serviceWorkerContainer.controller);
-  });
+  serviceWorkerContainer.addEventListener('controllerchange', (e) => {
+    console.log('controllerchange', e.target)
+    if (!serviceWorkerContainer.controller) return
+    registerClient(serviceWorkerContainer.controller)
+  })
 
   return new Promise((resolve, reject) => {
     /** @type {ServiceWorker | void} */
-    let serviceWorker;
+    let serviceWorker
     if (registration.installing) {
-      serviceWorker = registration.installing;
-      console.log("Installing service worker");
+      serviceWorker = registration.installing
+      console.log('Installing service worker')
     } else if (registration.waiting) {
-      serviceWorker = registration.waiting;
-      console.log("Waiting for service worker");
+      serviceWorker = registration.waiting
+      console.log('Waiting for service worker')
     } else if (registration.active) {
-      serviceWorker = registration.active;
-      console.log("Service worker active");
-      registerClient(serviceWorker);
-      return resolve(serviceWorkerContainer);
+      serviceWorker = registration.active
+      console.log('Service worker active')
+      registerClient(serviceWorker)
+      return resolve(serviceWorkerContainer)
     }
 
     if (!serviceWorker) {
-      return reject(new Error("Could not find Service Worker"));
+      return reject(new Error('Could not find Service Worker'))
     }
 
-    serviceWorker.addEventListener("statechange", onStateChange);
+    serviceWorker.addEventListener('statechange', onStateChange)
 
     /** @param {Event & { target: ServiceWorker }} e */
-    function onStateChange({ target: serviceWorker }) {
-      console.log("Service worker:", serviceWorker.state);
-      if (serviceWorker.state === "activated") {
+    function onStateChange ({ target: serviceWorker }) {
+      console.log('Service worker:', serviceWorker.state)
+      if (serviceWorker.state === 'activated') {
         // @ts-ignore
-        serviceWorker.removeEventListener("statechange", onStateChange);
-        registerClient(serviceWorker);
-        resolve(serviceWorkerContainer);
+        serviceWorker.removeEventListener('statechange', onStateChange)
+        registerClient(serviceWorker)
+        resolve(serviceWorkerContainer)
       }
     }
-  });
+  })
 }
